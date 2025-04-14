91d80885f7413a71d31f77b21b7d15a0ae223a20
enkaypeter <enkaypeter@gmail.com>
2025-04-14 20:06:39 +0100

    Add users migration

---
+++ student_service/src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  role VARCHAR(50) NOT NULL DEFAULT 'STUDENT'
+);
+
+CREATE INDEX idx_users_email ON users(email);
