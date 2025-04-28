508cd6c154de1e61c3d766af316e62ebd8655328
enkaypeter <enkaypeter@gmail.com>
2025-04-28 14:24:16 +0100

    :white_check_mark: unit and integration tests

--- student_service/src/main/java/dev/enkay/student_service/entity/Student.java
+++ student_service/src/main/java/dev/enkay/student_service/entity/Student.java
@@ -19,4 +19,10 @@ public class Student {
   @OneToOne
   @JoinColumn(name = "user_id")
   private User user;
+
+  @Column(name = "first_name")
+  private String firstName;
+  
+  @Column(name = "last_name")
+  private String lastName;
 }
