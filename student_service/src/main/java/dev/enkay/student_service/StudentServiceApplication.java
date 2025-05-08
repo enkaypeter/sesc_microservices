49db1a41b2457b14990693d615d2921ed8295ab1
enkaypeter <enkaypeter@gmail.com>
2025-05-08 16:03:07 +0100

    confirm graduation eligibility

--- student_service/src/main/java/dev/enkay/student_service/StudentServiceApplication.java
+++ student_service/src/main/java/dev/enkay/student_service/StudentServiceApplication.java
@@ -1,17 +1,19 @@
 package dev.enkay.student_service;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class StudentServiceApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		Dotenv dotenv = Dotenv.configure()
+			.ignoreIfMissing()
+			.load();
+		dotenv.entries()
+			.forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 
 		SpringApplication.run(StudentServiceApplication.class, args);
 	}
